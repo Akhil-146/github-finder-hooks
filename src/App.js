b988f37d239b2147b5f106bebb0a1cfdb556ace3
Akhil <akhil.tandrotu@gmail.com>
2021-08-05 11:50:05 +0530

    Add Github_Users_List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
+import axios from "axios";
+import NavBar from "./components/layout/NavBar";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
-        <h1>Hello World. Welcome to React</h1>
+        <NavBar title="Github Finder" />
+        <div className="container">
+          <Users users={users} loading={loading} />
+        </div>
       </div>
     );
   }
