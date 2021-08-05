75429183aaefa049a66c89420a7827214b9e0ddb
Akhil <akhil.tandrotu@gmail.com>
2021-08-05 12:22:10 +0530

    Add search field

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import NavBar from "./components/layout/NavBar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import "./App.css";
 
 class App extends Component {
@@ -18,12 +19,22 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+    console.log(this.state.users);
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className="App">
         <NavBar title="Github Finder" />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users users={users} loading={loading} />
         </div>
       </div>
